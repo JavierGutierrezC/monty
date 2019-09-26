76e3692e56b2d82f19af05bcea4c852c34eabf53
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-09-26 02:38:26 +0000

    first commit

---
+++ 0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+/**
+ * print_dlistint - prints all the elemenets of a list
+ * @h: pointer point to the list
+ *
+ *
+ * Return: Number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
