6c1e4ccf0f4346fabe79872e31ca5bb07d742345
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-09-26 01:25:18 +0000

    first commit

---
+++ 2-add_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * add_dnodeint - adds a node at the beginnig of a list
+ * @head: begining of the list
+ * @n: intiger to add to the list
+ * Return: Address of thenew element or NULL if it failed
+ *
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *newn;
+
+	newn = malloc(sizeof(dlistint_t));
+
+	if (newn == NULL)
+		return (NULL);
+	newn->n = n;
+	newn->prev = NULL;
+	newn->next = *head;
+	*head = newn;
+	if (newn->next != NULL)
+		(newn->next)->prev = newn;
+
+	return (newn);
+}
