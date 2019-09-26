b2092b32ce9d2ab22d1aea8d99d3c3d97a4c26c6
EC2 Default User <ec2-user@ip-172-31-91-220.ec2.internal>
2019-09-26 08:05:59 +0000

    created function add

--- functionsop2.c
+++ functionsop2.c
@@ -17,13 +17,38 @@ void nopop(stack_t **head, unsigned int count)
  */
 void addop(stack_t **head, unsigned int count)
 {
+	int addr = 0;
+	stack_t *current;
+	int n = 0;
+
 	if (*head != NULL)
-		print_dlistint(*head);
+	{
+	        current = *head;
+		while (current != NULL)
+		{
+			/* printf("%d\n", h->n); */
+			n++;
+			current = current->next;
+		}
+
+		if (n >= 2)
+		{
+			addr = (*head)->n;
+			delete_dnodeint_at_index(head, 0);
+			addr = addr + (*head)->n;
+			(*head)->n = addr;
+		}
+		else
+		{
+			dprintf(2, "L%d: can't add stack too short", count);
+			exit(EXIT_FAILURE);
+		}
+	}
 	else
-        {
-                dprintf(2, "L%d: can't add stack too short", count);
-                exit(EXIT_FAILURE);
-        }
+	{
+		dprintf(2, "L%d: can't add stack too short", count);
+		exit(EXIT_FAILURE);
+	}
 }
 /**
  * swapop - function to push int in a stack
